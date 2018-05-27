27f691aea29c4ce4c7d6c094465212ca99554542
mreeers <mr.eeers@mail.ru>
2018-05-27 15:30:06 +0500

    Доделана регистрация, код подтверждения по почте

--- src/main/java/com/example/sweater/controller/RegistrationController.java
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -4,7 +4,10 @@ import com.example.sweater.domain.User;
 import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
@@ -26,5 +29,17 @@ public class RegistrationController {
             return "registrator";
         }
         return "redirect:/login";
+    }
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code){
+        boolean isActivate = userService.activateUser(code);
+
+        if (isActivate){
+            model.addAttribute("message", "User successfully activated");
+        }
+        else {
+            model.addAttribute("message", "Activation code is not found");
+        }
+        return "login";
     }
 }
--- src/main/java/com/example/sweater/service/MailSender.java
+++ src/main/java/com/example/sweater/service/MailSender.java
@@ -1,6 +1,7 @@
 package com.example.sweater.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -9,11 +10,13 @@ import org.springframework.stereotype.Service;
 public class MailSender {
     @Autowired
     private JavaMailSender mailSender;
+    @Value("${spring.mail.username}")
+    private String username;
 
     public void send(String emailTo, String subject, String message){
         SimpleMailMessage mailMesage = new SimpleMailMessage();
 
-        mailMesage.setFrom("");
+        mailMesage.setFrom(username);
         mailMesage.setTo(emailTo);
         mailMesage.setSubject(subject);
         mailMesage.setText(message);
